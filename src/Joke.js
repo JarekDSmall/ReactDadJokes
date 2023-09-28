68892537e5138cb6794780a4aac44acf14aca5db
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-27 20:18:53 -0400

    Convert Joke Component to Functional Component
    - Replaced the class component syntax with a functional component.
    - Utilized the useState hook to manage the votes state.
    - Updated the upvote and downvote methods to use the setVotes function from useState.
    - Adjusted the render logic to use props and state variables directly.

--- src/Joke.js
+++ src/Joke.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
-import "./Joke.css";
+import React, { useState } from 'react';
+import './Joke.css';
 
-/** A single joke, along with vote up/down buttons. */
+function Joke({ text, votes }) {
+  const [currentVotes, setVotes] = useState(votes);
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
+  const upvote = () => {
+    setVotes(currentVotes + 1);
+  };
 
-    return (
-      <div className="Joke">
-        <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
-            <i className="fas fa-thumbs-up" />
-          </button>
+  const downvote = () => {
+    setVotes(currentVotes - 1);
+  };
 
-          <button onClick={evt => vote(id, -1)}>
-            <i className="fas fa-thumbs-down" />
-          </button>
-
-          {votes}
-        </div>
-
-        <div className="Joke-text">{text}</div>
+  return (
+    <div className="Joke">
+      <div className="Joke-buttons">
+        <button onClick={upvote}>Upvote</button>
+        <button onClick={downvote}>Downvote</button>
       </div>
-    );
-  }
+      <div className="Joke-text">{text}</div>
+      <div className="Joke-votes">{currentVotes}</div>
+    </div>
+  );
 }
 
 export default Joke;
